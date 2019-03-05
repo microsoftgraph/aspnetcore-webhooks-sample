e5d6e152aad1907ba9cbd4f28bce3fe8d4a661e7
Irvine Sunday <irvine.sunday@m-kopa.com>
2019-03-05 16:04:20 +0300

    Adding ignored file to source control

---
+++ src/GraphWebhooks-Core/Extensions/ClaimTypesExtension.cs
@@ -0,0 +1,15 @@
+﻿/*
+ *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ *  See LICENSE in the source repository root for complete license information.
+ */
+
+using System.Security.Claims;
+
+namespace GraphWebhooks_Core.Extensions
+{
+    public class ClaimTypesExtension
+    {
+        public const string ObjectIdentifier = "http://schemas.microsoft.com/identity/claims/objectidentifier";
+        public const string TenantId = "http://schemas.microsoft.com/identity/claims/tenantid";
+    }
+}

31620ad74f87ff33573b96e9e2499285ff5d0d03
Irvine Sunday <irvine.sunday@m-kopa.com>
2019-03-06 11:15:03 +0300

    Factory class for initializing authenticated graph clients

---
+++ src/GraphWebhooks-Core/Helpers/GraphServiceClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Graph;
+using System.Net.Http.Headers;
+
+namespace GraphWebhooks_Core.Helpers
+{
+    public static class GraphServiceClientFactory
+    {
+        public static GraphServiceClient GetAuthenticatedGraphClient(string accessToken)
+        {
+            return new GraphServiceClient(new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        // Append the access token to the request.
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue(
+                            Infrastructure.Constants.BearerAuthorizationScheme, accessToken);
+                    }));
+        }
+    }
+}

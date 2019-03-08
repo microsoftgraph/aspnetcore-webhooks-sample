40847520141ef16a74dbf9586e341a4d1cc44817
Irvine Sunday <irvine.sunday@m-kopa.com>
2019-03-08 16:48:29 +0300

    Added untracked file to git source control

---
+++ src/GraphWebhooks-Core/Helpers/Interfaces/ISubscriptionStore.cs
@@ -0,0 +1,14 @@
+﻿/*
+ *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ *  See LICENSE in the source repository root for complete license information.
+ */
+
+namespace GraphWebhooks_Core.Helpers.Interfaces
+{
+    public interface ISubscriptionStore
+    {
+        void SaveSubscriptionInfo(string subscriptionId, string clientState, string userId, string tenantId);
+
+        SubscriptionStore GetSubscriptionInfo(string subscriptionId);
+    }
+}
